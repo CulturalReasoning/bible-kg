38fe6760925078c1e066e48f82333548f02f69bb
张史小冰风 <zhangshibf@gmail.com>
2026-09-11 16:31:07 +0200

    Delete scripts/run_bws.py

--- scripts/run_bws.py
+++
@@ -1,6 +0,0 @@
-#I will assume the input file is a csv file with command delimiter and header as following
-#tuple_id,position,pair_id,reference,blixen_text,bible_text,BEST,WORST
-#BEST and WORST is flagged by x
-#this script will read the file, do BWS using the LLM, and save the response
-
-
